f0b78c7c49cf6622d45942c3b20df6ae856762b1
tanvir-tech <tanvirah325@gmail.com>
2023-05-28 11:50:46 +0600

    progress bar value show from score

--- resources/views/frontend/flashcard.blade.php
+++ resources/views/frontend/flashcard.blade.php
@@ -42,23 +42,32 @@
             </div>
         </div>
         {{-- progress bars start --}}
+
+        @php
+            use App\Models\Score;
+            use App\Models\Word;
+            $score = Score::where('wordlist_id', '=',$word[0]["wordlist_id"])
+                        ->where('user_id', '=', Auth::user()->id)
+                        ->first();
+            $wordcount = Word::where('wordlist_id', '=',$word[0]["wordlist_id"])->count();
+        @endphp
         <br>
 
         <label>Mastered: </label>
         <div class="progress">
-            <div id="mastered" class="progress-bar-striped bg-success" role="progressbar" style="width: 10%"
+            <div id="mastered" class="progress-bar-striped bg-success" role="progressbar" style="width: {{$score['known']/$wordcount*100}}%"
                 aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
         <br>
-        <label>Reviewing: </label>
+        <label>Reviewing:</label>
         <div class="progress">
-            <div id="reviewed" class="progress-bar-striped bg-warning" role="progressbar" style="width: 10%"
+            <div id="reviewed" class="progress-bar-striped bg-warning" role="progressbar" style="width: {{($score['known']+$score['unknown'])/$wordcount*100}}%"
                 aria-valuenow="10"></div>
         </div>
         <br>
         <label>Learning: </label>
         <div class="progress">
-            <div id="learning" class="progress-bar-striped bg-danger" role="progressbar" style="width: 10%"
+            <div id="learning" class="progress-bar-striped bg-danger" role="progressbar" style="width: {{$score['unknown']/$wordcount*100}}%"
                 aria-valuemin="0" aria-valuemax="100"></div>
         </div>
         <br>
